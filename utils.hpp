835012731ce19ffd23fefbc18dc4a46c29f0402f
Kugayama Nana <nonamea774@gmail.com>
2019-05-02 18:35:41 +0900

    use ref

--- utils.hpp
+++ utils.hpp
@@ -4,6 +4,6 @@
 #include <experimental/array>
 
 template<typename T, size_t N>
-bool in(T c, std::array<T, N> chars) {
-  return std::any_of(begin(chars), end(chars), [c](T t) { return c == t; });
+bool in(T const& c, std::array<T, N> const& chars) {
+  return std::any_of(begin(chars), end(chars), [&](T t) { return c == t; });
 }
