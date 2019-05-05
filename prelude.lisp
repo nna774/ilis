ccd59bc1af129fccfd9c228d111fbc6d7def7e01
Kugayama Nana <nonamea774@gmail.com>
2019-05-05 18:14:17 +0900

    fix null

--- prelude.lisp
+++ prelude.lisp
@@ -1,7 +1,7 @@
 (define cadr (lambda (x) (car (cdr x))))
 (define cddr (lambda (x) (cdr (cdr x))))
 
-(define null (lambda (x) (if (eq '() '()) #t #f)))
+(define null (lambda (x) (if (eq x '()) #t #f)))
 (define list (lambda xs xs))
 
 (define add (lambda (x y)
